f40a63d06c0d74638f94bde4d58e0c308c4a0f6c
vvataarne <is0399ii@ed.ritsumei.ac.jp>
2018-02-27 13:37:00 +0000

    Add TLE solution for C

---
+++ C/vv-TLE/main.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+int i;
+int n;
+int a[100000];
+int ans[100000];
+int c;
+map<int, int> m;
+
+int main() {
+  scanf("%d", &n);
+  for (i = 0; i < n; i++) scanf("%d", a + i);
+  for (i = 0; i < n; i++) {
+    m[a[i]]++;
+    m[a[n - 1 - i]]--;
+    int acc = 0;
+    for (auto &p : m) acc |= p.second;
+    if (acc == 0) ans[c++] = i + 1;
+  }
+  printf("%d", ans[0]);
+  for (i = 1; i < c; i++) printf(" %d", ans[i]);
+  printf("\n");
+}
