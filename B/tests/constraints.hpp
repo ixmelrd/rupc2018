414112bfe3b0680a4ecd7b2095504eedb869c4b3
vvataarne <is0399ii@ed.ritsumei.ac.jp>
2018-02-27 14:13:21 +0000

    Add B validator

---
+++ B/tests/constraints.hpp
@@ -0,0 +1,6 @@
+#define MIN_N 1
+#define MAX_N 100000
+#define MIN_a 1
+#define MIN_b 1
+#define MAX_a 1000000000
+#define MAX_b 1000000000
--- B/tests/validator.cc
+++ B/tests/validator.cc
@@ -1,2 +1,36 @@
+#include "./constraints.hpp"
+#include "./testlib.h"
+#include <iostream>
+#include <set>
+#define loop(i, a, n) for (int i = (a); i < (n); i++)
+#define rep(i, n) loop(i, 0, n)
+using namespace std;
+
 int main() {
+  registerValidation();
+  int N = inf.readInt(MIN_N, MAX_N);
+  inf.readEoln();
+  set<int> as;
+  rep(i, N) {
+    as.insert(inf.readInt(MIN_a, MAX_a));
+    if (i < N - 1) {
+      inf.readSpace();
+    } else {
+      inf.readEoln();
+    }
+  }
+  rep(i, N) {
+    inf.readInt(MIN_b, MAX_b);
+    if (i < N - 1) {
+      inf.readSpace();
+    } else {
+      inf.readEoln();
+    }
+  }
+  inf.readEof();
+
+  if (as.size() != N) {
+    cout << "Expected a_i is unique; Got duplication;" << endl;
+    return 1;
+  }
 }
