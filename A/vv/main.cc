0f019591693e28e7d8b0109f60e0d4c2575f6c7f
vvataarne <is0399ii@ed.ritsumei.ac.jp>
2018-02-28 12:40:06 +0000

    Fix A solution

--- A/vv/main.cc
+++ A/vv/main.cc
@@ -10,7 +10,7 @@ int main() {
   vector<int> a(n);
   for (int &x : a) cin >> x;
   rep(i, n - 1) loop(j, i + 1, n) {
-    if ((a[i] - a[j]) % 4) continue;
+    if ((a[i] - a[j]) % (n - 1)) continue;
     cout << a[i] << ' ' << a[j] << endl;
   }
 }
