2e90452efcb4e03333d2efecb73c8bd27f54d1e2
TMiya3 <is0283ir@ed.ritsumei.ac.jp>
2018-03-01 11:07:04 -0800

    DにT.Mの解答を追加

--- D/T.M/main.c
+++ D/T.M/main.c
@@ -8,29 +8,35 @@ int main(){
     scanf("%d",&s[i+1]);
     s[i+1]+=s[i];
   }
+  if(m==1){
+    printf("%.10lf\n",1.0*s[n]/n);
+    return 0;
+  }
   //s[n+1]=s[n];
   //for(i=0;i<=n+1;i++)printf("%d ",s[i]);printf("\n");
   for(i=1;i<n;i++){
     double e[510][510]={};
     for(j=0;j<m;j++){
-      if(i<j)continue;
+      if(i<=j)continue;
       for(k=1;k<=n;k++){
-	if(i<k)continue;//printf("!%d %d %d\n",i,j,k);
+	if(i<k+j)continue;//printf("!%d %d %d\n",i,j,k);
+	if(j==0&&i-k)continue;//printf("!%d %d %d\n",i,j,k);
 	e[j][k+1]=MAX(e[j][k+1],d[j][k]);
 	e[j+1][1]=MAX(e[j+1][1],d[j][k]+1.0*(s[i]-s[i-k])/(k));
       }
     }
     for(j=0;j<=m;j++){
       for(k=0;k<n;k++)d[j][k]=e[j][k];
-      //for(k=0;k<=n;k++)printf("%lf ",d[j][k]);printf("\n");
+      //for(k=0;k<=n;k++)printf("%10lf ",d[j][k]);printf("\n");
     }//printf("\n");
   }
-  for(i=n-m+1;i<=n;i++){
+  for(i=1;i<n-m+2;i++){
+    //printf("%lf %lf %lf\n",d[m-1][i]+1.0*(s[n]-s[n-i])/i,d[m-1][i],1.0*(s[n]-s[n-i])/(i));
     d[m][0]=MAX(d[m][0],d[m-1][i]+1.0*(s[n]-s[n-i])/i);
   }
   /*for(i=0;i<=m;i++){
     for(j=0;j<=n;j++)printf("%lf ",d[i][j]);printf("\n");
     }//*/
-  printf("%lf\n",d[m][0]);
+  printf("%.10lf\n",d[m][0]);
   return 0;
 }
