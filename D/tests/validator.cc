555332367e96b77d389c46bb9904c4a39f759d64
rika0384 <wakabayashirika@rika.local>
2018-03-03 00:50:54 +0900

    add D validator

--- D/tests/validator.cc
+++ D/tests/validator.cc
@@ -1,2 +1,31 @@
+#include "./testlib.h"
+#include "./constraints.hpp"
+#include<iostream>
+#include <cassert>
+
 int main() {
+
+  registerValidation();
+
+  int N = inf.readInt(MIN_N, MAX_N);
+
+  inf.readSpace();
+
+  inf.readInt(MIN_M, N);
+
+  inf.readEoln();
+
+  for(int i = 0; i < N ; i++){
+
+    inf.readInt(MIN_a, MAX_a);
+
+    if(i < N-1)
+      inf.readSpace();
+    else
+      inf.readEoln();
+
+  }
+
+  inf.readEof();
+
 }
