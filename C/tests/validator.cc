beb26e824e256533a4dd0148716e62ffe5c59604
rika0384 <wakabayashirika@rika.local>
2018-03-02 16:59:48 +0900

    add C validator

--- C/tests/validator.cc
+++ C/tests/validator.cc
@@ -1,2 +1,26 @@
+#include "./testlib.h"
+#include "./constraints.hpp"
+#include<iostream>
+#include <cassert>
+
 int main() {
+
+  registerValidation();
+
+  int n = inf.readInt(MIN_N, MAX_N);
+  inf.readEoln();
+
+  for(int i = 0; i < n ; i++){
+
+    inf.readInt(MIN_A, MAX_A);
+
+    if(i<n-1)
+      inf.readSpace();
+    else
+      inf.readEoln();
+
+  }
+
+  inf.readEof();
+
 }
