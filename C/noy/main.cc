0b9a84dee76fda984ccef1c8a78335ff64a368da
noy72 <kuma.tola3@gmail.com>
2018-03-02 17:29:13 +0900

    Fix C solution

--- C/noy/main.cc
+++ C/noy/main.cc
@@ -4,16 +4,32 @@
 #include <map>
 using namespace std;
 
+vector<int> compress(vector<int> v) {
+	sort(v.begin(), v.end());
+	v.erase(unique(v.begin(),v.end()),v.end());
+	return v;
+}
+
+int lb(vector<int>& v, int num){
+	return lower_bound(v.begin(), v.end(), num) - v.begin();
+}
+
 int main() {
 	int n;
 	cin >> n;
 
+	vector<int> in(n);
+	for (int i = 0; i < n; i++) {
+		cin >> in[i];
+	}
+	vector<int> com = compress(in);
+
 	vector<int> a(n);
 	for (int i = 0; i < n; i++) {
-		cin >> a[i];
+		a[i] = lb(com, in[i]); 
 	}
 
-	map<int, queue<int>> m;
+	vector<queue<int>> m(n);
 	for (int i = n - 1; i >= 0; i--) {
 		m[a[i]].push(n - 1 - i);
 	}
