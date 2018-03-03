86feb013377e799d5f67eac9d3bcf8e6ebbaae20
vvataarne <is0399ii@ed.ritsumei.ac.jp>
2018-03-03 14:04:10 +0000

    Update F solution

--- F/vv/main.cc
+++ F/vv/main.cc
@@ -7,116 +7,65 @@
 #define all(v) (v).begin(), (v).end()
 using namespace std;
 
-template<typename Monoid> constexpr typename Monoid::value_type powm(typename Monoid::value_type a, int b) {
-  Monoid m;
-  typename Monoid::value_type c = m.id();
-  while (b) {
-    if (b & 1) c = m(c, a);
-    b >>= 1;
-    a = m(a, a);
-  }
-  return c;
-}
-
-template<typename LSTTrait> class LazySegTree {
-  using FoldMonoid = typename LSTTrait::FoldMonoid;
-  using ActionMonoid = typename LSTTrait::ActionMonoid;
-  using T = typename FoldMonoid::value_type;
-  using Q = typename ActionMonoid::value_type;
-
-  LSTTrait lstTrait; // apply :: (T, Q) -> T
-  FoldMonoid foldM;
-  ActionMonoid actionM;
+template<typename T> class RangeUpdateSegTree {
   int size = 1;
-  std::vector<T> tree; // 1-indexed
-  std::vector<Q> lazy; // 1-indexed
-
-  void flush(int i, int l, int r) {
-    tree[i] = lstTrait.apply(tree[i], powm<ActionMonoid>(lazy[i], r - l));
-    if (i < size) {
-      lazy[i * 2] = actionM(lazy[i * 2], lazy[i]);
-      lazy[i * 2 + 1] = actionM(lazy[i * 2 + 1], lazy[i]);
-    }
-    lazy[i] = actionM.id();
-  }
+  size_t time = 1;
+  std::vector<pair<size_t, T>> tree; // 1-indexed
 
 public:
-  LazySegTree(const int n = 0) {
+  RangeUpdateSegTree(const int n = 0) {
     while (size < n) size *= 2;
-    tree.assign(size * 2, foldM.id());
-    lazy.assign(size * 2, actionM.id());
+    tree.resize(size * 2);
   }
 
-  template<typename InputIterator> LazySegTree(InputIterator first, InputIterator last) {
+  RangeUpdateSegTree(const int n, const T &x) {
+    while (size < n) size *= 2;
+    tree.assign(size * 2, {0, x});
+  }
+
+  template<typename InputIterator> RangeUpdateSegTree(InputIterator first, InputIterator last) {
     size_t n = std::distance(first, last);
     while (size < n) size *= 2;
-    tree.resize(size * 2, foldM.id());
-    lazy.resize(size * 2, actionM.id());
-    std::copy(first, last, tree.begin() + size);
-    for (int i = size - 1; i >= 1; i--) tree[i] = foldM(tree[i * 2], tree[i * 2 + 1]);
-  }
+    tree.resize(size * 2);
 
-  // [l, r)
-  void act(int l, int r, const Q x) { act(l, r, x, 1, 0, size); }
-  void act(const int l, const int r, const Q x, int i, int a, int b) {
-    flush(i, a, b);
-    if (b <= l || r <= a) return;
-    if (l <= a && b <= r) {
-      lazy[i] = actionM(lazy[i], x);
-      flush(i, a, b);
-      return;
+    auto result = tree.begin() + size;
+    while (first != last) {
+      result->second = *first;
+      ++result, ++first;
     }
-    act(l, r, x, i * 2, a, (a + b) / 2);
-    act(l, r, x, i * 2 + 1, (a + b) / 2, b);
-    tree[i] = foldM(tree[i * 2], tree[i * 2 + 1]);
   }
 
   // [l, r)
-  T fold(int l, int r) { return fold(l, r, 1, 0, size); }
-  T fold(int l, int r, int i, int a, int b) {
-    flush(i, a, b);
-    if (b <= l || r <= a) return foldM.id();
-    if (l <= a && b <= r) return tree[i];
-    T lhs = fold(l, r, i * 2, a, (a + b) / 2);
-    T rhs = fold(l, r, i * 2 + 1, (a + b) / 2, b);
-    tree[i] = foldM(tree[i * 2], tree[i * 2 + 1]);
-    return foldM(lhs, rhs);
+  void update(int l, int r, const T &x) {
+    for (l += size, r += size; l < r; l /= 2, r /= 2) {
+      if (l & 1) tree[l++] = {time, x};
+      if (r & 1) tree[--r] = {time, x};
+    }
+    time++;
   }
-};
 
-template<typename T, T upperInf = std::numeric_limits<T>::max()> struct minMonoid {
-  using value_type = T;
-  constexpr value_type id() { return upperInf; }
-  constexpr value_type operator()(const value_type &a, const value_type &b) { return a < b ? a : b; }
-};
-
-template<typename T> struct updateMonoid {
-  using value_type = pair<bool, T>;
-  constexpr value_type id() { return {false, T()}; }
-  constexpr value_type operator()(const value_type &a, const value_type &b) { return b.first ? b : a; }
-};
-
-template<typename T, T upperInf = std::numeric_limits<T>::max()> struct RMQ_RUQ_Trait {
-  using FoldMonoid = minMonoid<T, upperInf>;
-  using ActionMonoid = updateMonoid<T>;
-  typename FoldMonoid::value_type apply(typename FoldMonoid::value_type t, typename ActionMonoid::value_type q) { return q.first ? q.second : t; }
+  const T operator[](int i) const {
+    pair<size_t, T> p = tree[i += size];
+    while (i /= 2) p = max(p, tree[i]);
+    return p.second;
+  }
 };
 
 class RangeUnionFind {
-  LazySegTree<RMQ_RUQ_Trait<int>> lst;
+  RangeUpdateSegTree<int> rust;
 
 public:
   RangeUnionFind(const int n) {
     vector<int> ufInit(n);
-    iota(ufInit.begin(), ufInit.end(), 0);
-    lst = LazySegTree<RMQ_RUQ_Trait<int>>(ufInit.begin(), ufInit.end());
+    iota(all(ufInit), 0);
+    rust = RangeUpdateSegTree<int>(all(ufInit));
   }
 
   int root(int x) {
-    int a = lst.fold(x, x + 1);
+    int a = rust[x];
     if (a == x) return x;
     int b = root(a);
-    lst.act(x, x + 1, {true, b});
+    rust.update(x, x + 1, b);
     return b;
   }
 
@@ -125,7 +74,7 @@ public:
   void unite(int a, int b) { // unite [a, b)
     int r = root(a);
     if (a == b) return;
-    lst.act(a, b, {true, r});
+    rust.update(a, b, r);
   }
 };
 
