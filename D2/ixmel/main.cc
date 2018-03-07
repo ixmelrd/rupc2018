e2688416b231db6646baec15d78e7f6cd23d955f
ixmel <kazuyakazuya524@gmail.com>
2018-03-07 17:53:54 +0900

    ixmelD2追加

--- D2/ixmel/main.cc
+++ D2/ixmel/main.cc
@@ -1,2 +1,106 @@
-int main() {
+// g++ -std=c++11 a.cpp
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>	
+#include<map>
+#include<set>
+#include<utility>
+#include<cmath>
+#include<random>
+#include<cstring>
+#include<queue>
+#include<stack>
+#include<bitset>
+#include<cstdio>
+#include<sstream>
+#include<iomanip>
+#include<assert.h>
+#include<typeinfo>
+#define loop(i,a,b) for(int i=a;i<b;i++) 
+#define rep(i,a) loop(i,0,a)
+#define pb push_back
+#define all(in) in.begin(),in.end()
+#define shosu(x) fixed<<setprecision(x)
+using namespace std;
+//kaewasuretyuui
+typedef long long ll;
+//#define int ll
+typedef int Def;
+typedef pair<Def,Def> pii;
+typedef vector<Def> vi;
+typedef vector<vi> vvi;
+typedef vector<pii> vp;
+typedef vector<vp> vvp;
+typedef vector<string> vs;
+typedef vector<double> vd;
+typedef vector<vd> vvd;
+typedef pair<Def,pii> pip;
+typedef vector<pip>vip;
+// #define mt make_tuple
+//typedef tuple<pii,int,int> tp;
+//typedef vector<tp> vt;
+template<typename A,typename B>bool cmin(A &a,const B &b){return a>b?(a=b,true):false;}
+template<typename A,typename B>bool cmax(A &a,const B &b){return a<b?(a=b,true):false;}
+//template<class C>constexpr int size(const C &c){return (int)c.size();}
+//template<class T,size_t N> constexpr int size(const T (&xs)[N])noexcept{return (int)N;}
+const double PI=acos(-1);
+const double EPS=1e-7;
+Def inf = sizeof(Def) == sizeof(long long) ? 2e18 : 1e9+10;
+int dx[]={0,1,0,-1};
+int dy[]={1,0,-1,0};
+class DIJ{
+	public:
+	struct edge{
+		int to,cost;
+	};
+	vector<vector<edge> >G;
+	int n;
+	vi d;//distance
+	DIJ(int size){
+		n=size;
+		G=vector<vector<edge> >(n);
+	}
+	void add_edge(int a,int b,int c){
+		edge e={b,c},ee={a,c};
+		G[a].pb(e);
+		G[b].pb(ee);
+	}
+	void dij(int s){
+		d=vi(n,inf);
+		d[s]=0;
+		priority_queue<pii>q;
+		q.push(pii(0,s));
+		while(!q.empty()){
+			pii p=q.top();
+			q.pop();
+			int pos=p.second,cost=-p.first;
+			if(cost>d[pos])continue;
+			rep(i,G[pos].size()){
+				edge e=G[pos][i];
+				int to=e.to;
+				int nowcost=cost+e.cost;
+				if(nowcost<d[to]){
+					d[to]=nowcost;
+					q.push(pii(-d[to],to));
+				}
+			}
+		}
+	}
+};
+int main(){
+	int n,m,s,t;
+	cin>>n>>m>>s>>t;
+	s--;t--;
+	DIJ dij(n);
+	while(m--){
+		int a,b,c,d;
+		cin>>a>>b>>c>>d;
+		a--;b--;c--;
+		dij.add_edge(a,b,d);
+		dij.add_edge(b,c,d);
+		dij.add_edge(c,a,d);
+	}
+	dij.dij(s);
+	cout<<dij.d[t]<<endl;
 }
