4cdea465e02f0b92d3a290d8a909d6889bc21d34
vvataarne <is0399ii@ed.ritsumei.ac.jp>
2018-03-12 02:47:33 +0000

    Fix TLE solution for D2

--- D2/vv-TLE/main.cc
+++ D2/vv-TLE/main.cc
@@ -1,37 +1,39 @@
 #include <iostream>
 #include <queue>
 #include <tuple>
+#define int long long
 #define loop(i, a, n) for (i = (a); i < (n); i++)
 #define rep(i, n) loop(i, 0, n)
 constexpr int INF = 1000000000;
+constexpr int perm[3][2] = {{0, 1}, {1, 2}, {2, 0}};
+constexpr int pow3[12] = {1, 3, 9, 27, 81, 243, 729, 2187, 6561, 19683, 59049, 177147};
 using namespace std;
 
 using Graph = vector<vector<pair<int, int>>>;
 using state = pair<int, int>;
 
 int n, m, s, t;
-int e[2000][3];
+int e[2000][4];
 int i, j;
-int perm[6][3] = {{0, 1, 2}, {0, 2, 1}, {1, 0, 2}, {1, 2, 0}, {2, 0, 1}, {2, 1, 0}};
-int pow6[8] = {1, 6, 36, 216, 1296, 7776, 46656, 279936};
 Graph g;
 priority_queue<state, vector<state>, greater<state>> q;
 vector<int> dist;
 int d, v;
 int mini = INF;
 
-int main() {
-  scanf("%d %d %d %d", &n, &m, &s, &t);
-  rep(i, m) rep(j, 3) scanf("%d", &e[i][j]);
-  if (m > 7) {
+signed main() {
+  scanf("%lld %lld %lld %lld", &n, &m, &s, &t);
+  rep(i, m) rep(j, 4) scanf("%lld", &e[i][j]);
+  if (m > 11) {
     for (;;) {}
   }
   --s, --t;
-  rep(i, pow6[m]) {
+  rep(i, pow3[m]) {
     g = Graph(n);
     rep(j, m) {
-      int p = i / pow6[j] % 6;
-      g[e[j][perm[p][0]] - 1].emplace_back(e[j][perm[p][1]] - 1, e[j][perm[p][2]]);
+      int p = i / pow3[j] % 3;
+      g[e[j][perm[p][0]] - 1].emplace_back(e[j][perm[p][1]] - 1, e[j][3]);
+      g[e[j][perm[p][1]] - 1].emplace_back(e[j][perm[p][0]] - 1, e[j][3]);
     }
     q = priority_queue<state, vector<state>, greater<state>>();
     dist = vector<int>(n, INF);
@@ -48,7 +50,7 @@ int main() {
         }
       }
     }
-    mini = min(mini, dist[t]);
+    if (mini > dist[t]) mini = dist[t];
   }
-  printf("%d\n", mini);
+  printf("%lld\n", mini);
 }
