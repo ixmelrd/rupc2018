0e2fff1a687c60e98de7c340be1eb0764b5b3610
TMiya3 <is0283ir@ed.ritsumei.ac.jp>
2018-03-13 21:17:58 +0900

    Eのgeneratorを僅かに修正

--- E/tests/generator.c
+++ E/tests/generator.c
@@ -21,7 +21,7 @@ void challenge00(){
   out(d,s);
 }
 int main() {
-  int i,n;
+  int i,n,a;
   char d[10010],s[100],g[5]={"5103"};
 
   for(i=0;i<10;i++){
@@ -53,19 +53,31 @@ int main() {
   }
 
   for(i=0;i<10;i++){
-    sprintf(s,"54_51?3_%02d.in",i);
+    sprintf(s,"54_51-3_%02d.in",i);
     strcpy(d,g);
     d[2]+=i;
     out(d,s);
   }
 
   for(i=0;i<10;i++){
-    sprintf(s,"55_51?2_%02d.in",i);
+    sprintf(s,"55_51-2_%02d.in",i);
     strcpy(d,g);
     d[2]+=i;
     d[3]--;
     out(d,s);
   }
+
+  for(i=0;i<10;i++){
+    n=rnd.next(MIN_digits_of_N,MAX_digits_of_N);
+    sprintf(s,"56_in_51-3_%02d.in",i);
+    MakeNum(n,d);
+    a=rnd.next(0,n-4);
+    d[a  ]='5';
+    d[a+1]='1';
+    d[a+3]='3';
+    out(d,s);
+  }
+  
   challenge00();
   return 0;
 }
