f613021dc505e8f7417da35ca1d65880dd3636d8
TMiya3 <is0283ir@ed.ritsumei.ac.jp>
2018-03-14 04:34:26 +0900

    BにT.Mの解答を追加

---
+++ B/T.M2/main.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+  int a[100010],b,n,i,s=0,u[100010];
+  scanf("%d",&n);
+  for(i=0;i<n;i++)scanf("%d",&a[i]);
+  for(i=0;i<n;i++){
+    scanf("%d",&b);
+    s+=u[b];
+    u[b]=1;
+  }
+  if(s==0){
+    printf("Yes\n");
+    return 0;
+  }
+  for(i=0;i<n;i++)printf("%d%c",u[a[i]]+1,n-i-1?' ':'\n');
+  for(i=0;i<n;i++)printf("2%c",n-i-1?' ':'\n');
+  return 0;
+}
