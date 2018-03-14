07d0e44a412f616ba869fc8c20a92cb3ebe574cb
TMiya3 <is0283ir@ed.ritsumei.ac.jp>
2018-03-14 20:35:00 +0900

    BのT.M解を修正

--- B/T.M2/main.c
+++ B/T.M2/main.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main(){
-  int a[100010],b,n,i,s=0,u[100010];
+  int a[100010],b,n,i,s=0,u[100010]={};
   scanf("%d",&n);
   for(i=0;i<n;i++)scanf("%d",&a[i]);
   for(i=0;i<n;i++){
@@ -12,6 +12,7 @@ int main(){
     printf("Yes\n");
     return 0;
   }
+  printf("No\n");
   for(i=0;i<n;i++)printf("%d%c",u[a[i]]+1,n-i-1?' ':'\n');
   for(i=0;i<n;i++)printf("2%c",n-i-1?' ':'\n');
   return 0;
