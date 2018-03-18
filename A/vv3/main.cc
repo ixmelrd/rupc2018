1206059f21c4a43527f4af21963f9b48a40730a8
vvataarne <is0399ii@ed.ritsumei.ac.jp>
2018-03-18 13:56:46 +0000

    Add A solution

---
+++ A/vv3/main.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#define loop(i, a, n) for (int i = (a); i < (n); i++)
+#define rep(i, n) loop(i, 0, n)
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  for (int &x : a) cin >> x;
+  rep(i, n - 1) loop(j, i + 1, n) {
+    if ((a[i] % (n - 1)) != (a[j] % (n - 1))) continue;
+    cout << a[i] << ' ' << a[j] << endl;
+    return 0;
+  }
+}
